82aa4cd447d1686a3f8c43aa11fac33e3782a08c
anjanikumar <anjanikumar1802@gmail.com>
2018-07-09 00:23:42 +0530

    First Commit 1) Splash Screen added with dummy image

---
+++ app/src/main/java/com/example/anjanikumar/sanchi/java/Home.java
@@ -0,0 +1,16 @@
+package com.example.anjanikumar.sanchi.java;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.anjanikumar.sanchi.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
+
